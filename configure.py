aa9f46334186c0f576a874db5a0c12a2aa62250d
Chau Van Sang <chauvansang97@gmail.com>
2021-01-12 23:28:07 +0700

    remove unneccessary file

--- configure.py
+++ configure.py
@@ -10,16 +10,16 @@ class Configure:
             self.transform = np.loadtxt("transform.txt", dtype=np.float32)
             self.first_depth = np.loadtxt("first_depth.txt", dtype=np.float32)
         except Exception as e:
+            print(e)
             self.transform = np.float32([[0, 0], [512, 0], [0, 424], [512, 424]])
             self.first_depth = None
             np.savetxt("transform.txt", self.transform)
-            print(e)
         self.config = configparser.ConfigParser()
         if os.path.exists('config_kinect.ini') is False:
             self.config['server.com'] = {}
             top_secret = self.config['server.com']
             top_secret['min_depth'] = self.min_depth
-            top_secret['ip'] = "127.0.0.1"
+            top_secret['server_ip'] = "127.0.0.1"
             top_secret['area'] = '30'
             top_secret['max_depth'] = '0'
             top_secret['kernel'] = '0'
@@ -31,7 +31,7 @@ class Configure:
         else:
             self.config.read('config_kinect.ini')
             self.min_depth = self.config['server.com']['min_depth']
-            self.ip = self.config['server.com']['ip']
+            self.server_ip = self.config['server.com']['server_ip']
             self.area = self.config['server.com']['area']
             self.max_depth = self.config['server.com']['max_depth']
             self.kernel = self.config['server.com']['kernel']
--- main.py
+++ main.py
@@ -2,24 +2,12 @@ from queue import Queue
 
 from configure import Configure
 from kinect_worker import KinectWorker
-from udp_server import KinectWebSocket, UdpServer
+from udp_server import KinectWebSocket
 
 if __name__ == "__main__":
     configure = Configure()
     queue = Queue()
     kinect_worker = KinectWorker(configure=configure, queue=queue)
-
-    # kinect_server = UdpServer(configure=configure, queue=queue)
-    kinect_server = KinectWebSocket(queue=queue)
+    kinect_server = KinectWebSocket(queue=queue, configure=configure)
     kinect_server.start()
-    # new_loop = asyncio.new_event_loop()
-    # start_server = websockets.serve(kinect_server.handler,  '0.0.0.0', configure.port, loop=new_loop)
-    # t = Thread(target=kinect_server.start_loop, args=(new_loop, start_server))
-    # t.start()
-    # while True:
-    #     t = 0
     kinect_worker.run()
-    # ws_server = websockets.serve(kinect_server.handler, '0.0.0.0', configure.port)
-    # loop = asyncio.get_event_loop()
-    # loop.run_until_complete(ws_server)
-    # loop.run_forever()
